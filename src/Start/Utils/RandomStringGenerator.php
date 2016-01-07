74e82778c3e77ac5809c1ff8b901f59e90485ff2
Lennart Rosam <hello@takuto.de>
2016-01-07 01:13:46 +0100

    Add RandomString generator and begin work on entities

---
+++ src/Start/Utils/RandomStringGenerator.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ *  Start! - An open source Doodle.com clone
+ *
+ *  Copyright (C) 2016  Lennart Rosam <hello@takuto.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+namespace Start\Utils;
+
+/**
+ * Class RandomStringGenerator
+ *
+ * Solution taken from here:
+ * http://stackoverflow.com/a/13733588/1056679
+ */
+class RandomStringGenerator
+{
+    /** @var string */
+    protected $alphabet;
+
+    /** @var int */
+    protected $alphabetLength;
+
+
+    /**
+     * @param string $alphabet
+     */
+    public function __construct($alphabet = '')
+    {
+        if ('' !== $alphabet) {
+            $this->setAlphabet($alphabet);
+        } else {
+            $this->setAlphabet(
+                implode(range('a', 'z'))
+                . implode(range('A', 'Z'))
+                . implode(range(0, 9))
+            );
+        }
+    }
+
+    /**
+     * @param string $alphabet
+     */
+    public function setAlphabet($alphabet)
+    {
+        $this->alphabet = $alphabet;
+        $this->alphabetLength = strlen($alphabet);
+    }
+
+    /**
+     * @param int $length
+     * @return string
+     */
+    public function generate($length)
+    {
+        $token = '';
+
+        for ($i = 0; $i < $length; $i++) {
+            $randomKey = $this->getRandomInteger(0, $this->alphabetLength);
+            $token .= $this->alphabet[$randomKey];
+        }
+
+        return $token;
+    }
+
+    /**
+     * @param int $min
+     * @param int $max
+     * @return int
+     */
+    protected function getRandomInteger($min, $max)
+    {
+        $range = ($max - $min);
+
+        if ($range < 0) {
+            // Not so random...
+            return $min;
+        }
+
+        $log = log($range, 2);
+
+        // Length in bytes.
+        $bytes = (int) ($log / 8) + 1;
+
+        // Length in bits.
+        $bits = (int) $log + 1;
+
+        // Set all lower bits to 1.
+        $filter = (int) (1 << $bits) - 1;
+
+        do {
+            $rnd = hexdec(bin2hex(openssl_random_pseudo_bytes($bytes)));
+
+            // Discard irrelevant bits.
+            $rnd = $rnd & $filter;
+
+        } while ($rnd >= $range);
+
+        return ($min + $rnd);
+    }
+}
